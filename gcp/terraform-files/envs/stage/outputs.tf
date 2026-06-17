8b14a18bc3cb93335eaad3d5c400bf4585c4a2e2
MadhavSake <109482275+MadhavSake@users.noreply.github.com>
2026-06-17 11:52:48 +0530

    Create outputs.tf

---
+++ gcp/terraform-files/envs/stage/outputs.tf
@@ -0,0 +1,79 @@
+######################################
+# 🌍 Global Outputs (Example)
+######################################
+
+# Uncomment or add output blocks below as needed
+
+# output "project_id" {
+#   value = var.project_id
+# }
+
+# output "region" {
+#   value = var.region
+# }
+
+# output "location" {
+#   value = var.location
+# }
+
+######################################
+# ☸️ GKE Module Outputs (Example)
+######################################
+
+# output "cluster_name" {
+#   value = var.cluster_name
+# }
+
+# output "machine_type" {
+#   value = var.machine_type
+# }
+
+######################################
+# 🔌 VPC Module Outputs (Example)
+######################################
+
+# output "vpc_name" {
+#   value = var.vpc_name
+# }
+
+# output "subnet_name" {
+#   value = var.subnet_name
+# }
+
+# output "subnet_cidr" {
+#   value = var.subnet_cidr
+# }
+
+######################################
+# 🔥 Firewall / Networking
+######################################
+
+# output "ssh_source_ip" {
+#   value = var.ssh_source_ip
+# }
+
+######################################
+# 📦 Artifact Registry
+######################################
+
+# output "repo_name" {
+#   value = var.repo_name
+# }
+
+######################################
+# ☁️ Cloud Storage
+######################################
+
+# output "bucket_name" {
+#   value = var.bucket_name
+# }
+
+######################################
+# 🔌 Project Services
+######################################
+
+# output "enabled_apis" {
+#   value = var.enabled_apis
+# }
+
+# Remove this file entirely if you don't need to output anything
